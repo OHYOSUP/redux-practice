2ce82c5ce56644bf85bc15db949c075d4bf00096
Yo sup <dhdytjq2@gamil.com>
2023-09-11 00:08:26 +0900

    redux todo list

--- src/index.js
+++ src/index.js
@@ -1,21 +1,78 @@
-const todoForm = document.querySelector("form")
-const todoInput = document.querySelector("input")
-const todoList = document.querySelector("ul")
+const { createStore } = require("redux");
+const todoForm = document.querySelector("form");
+const todoInput = document.querySelector("input");
+const todoList = document.querySelector("ul");
 
-const createTodo = (value)=>{
-  const li = document.createElement('li')
-  li.innerText = value
-  todoList.appendChild(li)
-}
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-const handleSubmit = (e)=>{
-  e.preventDefault()
-  const value = todoInput.value
-  todoInput.value= ""
-  console.log(value)
-  createTodo(value)
-}
+const addTodo = (value) => {
+  return {
+    type: ADD_TODO,
+    text: value,
+  };
+};
 
+const deleteTodo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
+
+const dispatchAddTodo = (value) => {
+  store.dispatch(addTodo(value));
+};
+const dispatchDeleteTodo = (e) => {
+  
+  const id = e.target.parentNode.id;
+  console.log(id)
+  store.dispatch(deleteTodo(id));
+};
+
+const reducer = (state = [], action) => {    
+  switch (action.type) {
+    case ADD_TODO:
+      return [{ text: action.text, id: Date.now() }, ...state];
+    case DELETE_TODO:
+      return state.filter(todo => Number(todo.id) !== Number(action.id));
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
+
+// const deleteTodo = (e) => {
+//   const id = e.target.parentNode.id;
+//   dispatchDeleteTodo(id);
+//   console.log(e.target.parentNode.id);
+// };
+
+
+
+const paintTodo = () => {
+  const todos = store.getState();  
+  todoList.innerHTML = "";
+  todos.forEach((todo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.addEventListener("click", dispatchDeleteTodo);
+    li.id = todo.id;
+    li.innerText = todo.text;
+    btn.innerText = "DEL";
+    li.appendChild(btn);
+    todoList.appendChild(li);
+  });
+};
+
+store.subscribe(paintTodo);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const value = todoInput.value;
+  todoInput.value = "";
+  dispatchAddTodo(value);
+};
 
 todoForm.addEventListener("submit", handleSubmit);
 
